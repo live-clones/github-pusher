186339c503705c4d8af75e752a51811dc0a877fd
Nico Schlömer <nico.schloemer@gmail.com>
2014-08-27 11:03:48 +0200

    use github:trilinos/trilinos

--- trilinos.sh
+++ trilinos.sh
@@ -2,12 +2,12 @@
 
 # Preparation:
 #   $ GIT_SSL_NO_VERIFY=1 git clone https://software.sandia.gov/trilinos/repositories/publicTrilinos
-#   $ git remote add github git@github.com:nschloe/trilinos.git
+#   $ git remote add github git@github.com:trilinos/trilinos.git
 
 # Set SSH agent variables.
 eval $(cat $HOME/.ssh/agent/info)
 
 cd ${HOME}/software/trilinos/publicTrilinos/
-#GIT_SSL_NO_VERIFY=1 git pull origin master -q || exit 1
-GIT_SSL_NO_VERIFY=1 git fetch -p origin || exit 1
+GIT_SSL_NO_VERIFY=1 git pull origin master -q || exit 1
+#GIT_SSL_NO_VERIFY=1 git fetch -p -q origin || exit 1
 git push github master --tags -q || exit 1
