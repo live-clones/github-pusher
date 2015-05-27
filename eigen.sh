72263fd43087774f5aae3fb1126cab9d177b0b8f
Nico Schlömer <nico.schloemer@gmail.com>
2015-05-27 10:17:55 +0200

    added eigen pusher

---
+++ eigen.sh
@@ -0,0 +1,30 @@
+#!/bin/sh -ue
+#
+# Preparation (see <http://stackoverflow.com/a/1089221/353337>):
+#   $ sudo apt install mercurial-git
+#   Add 
+#     [extensions]
+#     hgext.bookmarks =
+#     git =
+#   to your ~/.hgrc.
+#   $ cd ~/software/eigen/
+#   $ hg clone https://bitbucket.org/eigen/eigen/ upstream
+#   $ cd upstream
+#   $ hg bookmark -r default master
+#   $ Add
+#     [git]
+#     intree = true
+#   to .hg/hgrc (in the repo).
+#   $ hg gexport # may take some time
+#   $ git remote add github git@github.com:live-clones/eigen.git
+#
+
+# Set SSH agent variables.
+eval $(cat $HOME/.ssh/agent/info)
+
+cd ${HOME}/software/eigen/upstream/
+# Pull from BitBucket and merge it into git
+hg pull -q
+hg gexport
+# Push to GitHub.
+git push github master --tags -q
