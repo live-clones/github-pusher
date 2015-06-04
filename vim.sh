e6b2fc290c1fdab616e751de2baa8b713170db4d
Nico Schlömer <nico.schloemer@gmail.com>
2015-06-04 14:57:46 +0200

    use hgexport for vim

--- vim.sh
+++ vim.sh
@@ -1,19 +1,30 @@
 #!/bin/sh -ue
 #
-# Preparation:
-#   Install git-hg <https://github.com/cosmin/git-hg> and make sure that the
-#   ./bin/ directory is in the path.
+# Preparation (see <http://stackoverflow.com/a/1089221/353337>):
+#   $ sudo apt install mercurial-git
+#   Add
+#     [extensions]
+#     hgext.bookmarks =
+#     git =
+#   to your ~/.hgrc.
 #   $ cd ~/software/vim/
-#   $ git-hg clone https://code.google.com/p/vim/ git-hg/
-#   $ cd git-hg
+#   $ hg clone https://vim.googlecode.com/hg/ upstream
+#   $ cd upstream
+#   $ hg bookmark -r default master
+#   $ Add
+#     [git]
+#     intree = true
+#   to .hg/hgrc (in the repo).
+#   $ hg gexport # may take some time
 #   $ git remote add github git@github.com:live-clones/vim.git
 #
 
 # Set SSH agent variables.
 eval $(cat $HOME/.ssh/agent/info)
 
-cd ${HOME}/software/vim/git-hg/
-# Pull from BitBucket.
-git-hg pull
+cd ${HOME}/software/vim/upstream/
+# Pull from BitBucket and merge it into git
+hg pull -q
+hg gexport
 # Push to GitHub.
 git push github master --tags -q
