a73963b61bdecabb65b392674d6bf2caed44eac6
Nico Schlömer <nico.schloemer@gmail.com>
2021-03-09 13:28:25 +0000

    add paraview

--- git2git.sh
+++ git2git.sh
@@ -98,6 +98,10 @@ clone2git \
   "https://bitbucket.org/blaze-lib/blaze.git" \
   "git@github.com:live-clones/blaze.git"
 
+clone2git \
+  "https://gitlab.kitware.com/paraview/paraview.git" \
+  "git@github.com:live-clones/paraview.git"
+
 
 # Next: Mercurial/hg
 # For this to work, git-remote-hg <https://github.com/felipec/git-remote-hg>
